238dcd64cef028ac578096c12239630edee7e8db
Aman13 <sandhu.amandeep.s@gmail.com>
2014-10-25 14:54:13 -0700

    Working Prototype, retrieves information off of git, sends emails using mandrill.

--- bin/changelog.js
+++ bin/changelog.js
@@ -5,20 +5,27 @@ var log = require('../lib/log'),
 
 var fs = require('fs');
 
+var mandrill = require('mandrill-api/mandrill');
+
 var child_process = require('child_process');
 
 var location = '/home/amansandhu/Documents/Projects/repo-log-test';
 
+var locationurl = 'https://github.com/Aman13/repo-log-test/pull/';
+
 var Promise = require('rsvp').Promise;
 
 var util = require('util');
+
+mandrill_client = new mandrill.Mandrill('5oM9eU13RLqrIbo3YSDgEg');
+
 
 function tagForCommit(sha1) {
 	return new Promise(function(resolve, reject) {
 		child_process.execFile('git', ['tag', '--points-at', sha1], {cwd: location}, function(err, tag) {
 			if (err) return reject(err);
 			else return resolve(tag.split('\n').slice(0, -1));
-		});	
+		});
 	});
 }
 
@@ -54,10 +61,14 @@ log(location, argv.range, function(err, commits){
 		var sub = _.chain(tree.children).map(function(child) {
 			return '<li>' + message(child, depth+1) + '</li>';
 		}).join('').value();
-		tree.commit.msg.split('\n');
 		if(!tree.commit) return '<ul>' + sub + '</ul>';
-		//return tree.commit.commit + '<br/>' + tree.commit.author + '<ul>' + sub + '</ul>';
-		return _.template ('${author} <br/> ${msg}', tree.commit) + '<ul>' + sub + '</ul>';
+		tree.commit.msg = tree.commit.msg.split('\n');
+		var expr = /^Merge pull request #(\d+) from ([^\s]+)/;
+		expr = expr.exec(tree.commit.msg[0]);
+		var pullUrl = locationurl + expr[1];
+	//	return tree.commit.commit + '<br/>' + tree.commit.author + '<ul>' + sub + '</ul>';
+	//	return _.template ('${author} <br/> ${msg}', tree.commit) + '<ul>' + sub + '</ul>';
+		return _.template ('${author} <br/>',tree.commit) + '<a href="'+pullUrl+'">details</a>' + _.template ('<br/> <b>Commit message:</b> <br/> ${msg[2]}', tree.commit) + '<ul>' + sub + '</ul>';
 	}
 
 
@@ -74,6 +85,25 @@ log(location, argv.range, function(err, commits){
 	}).then(function() {
 		var tree = { children: [] };
 		buildTree(commits[0], 0, tree);
-		console.log(message(tree, 0));
+		var info = message(tree, 0);
+
+		var email = {
+			'html': info,
+			'subject': 'example commit log',
+			'from_email': 'sandhu.amandeep.s@gmail.com',
+			'from_name': 'Aman',
+			'to': [{
+				'email': 'scorpion_013@hotmail.com',
+				'name': 'Bob',
+				'type': 'to'
+			}]
+
+		};
+
+		mandrill_client.messages.send({message: email}, function(result) {
+			console.log(result);
+		}, function(err) {
+			console.log('mistakes were made');
+		});
 	})
 });
